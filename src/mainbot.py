4072c96b3a2d134dbfb9f9b696663b52f5da3943
nekosune <kat.swales@nekokittygames.com>
2013-07-15 15:01:18 +0100

    Added basic update logic

--- src/mainbot.py
+++ src/mainbot.py
@@ -1,13 +1,31 @@
-import gdata.docs.service
+from yrconfigparser import MyConfigParser
+import os
+import sys
+import praw
+from datetime import datetime
+import pickle
 
-# Create a client class which will make HTTP requests with Google Docs server.
-client = gdata.docs.service.DocsService()
-# Authenticate using your Google Docs email address and password.
-client.ClientLogin('kat.swales@nekokittygames.com', 'kit1sune')
+cfg_file=MyConfigParser()
+path_to_cfg = os.path.abspath(os.path.dirname(sys.argv[0]))
+path_to_cfg = os.path.join(path_to_cfg, 'ytconfig.cfg')
+cfg_file.read(path_to_cfg)
 
-# Query the server for an Atom feed containing a list of your documents.
-documents_feed = client.GetDocumentListFeed()
-# Loop through the feed and extract each document entry.
-for document_entry in documents_feed.entry:
-  # Display the title of the document on the command line.
-  print document_entry.title.text
+r = praw.Reddit(user_agent=cfg_file.get('reddit', 'user_agent'))
+print 'Logging in as '+ cfg_file.get('reddit', 'username')
+r.login(cfg_file.get('reddit', 'username'),
+            cfg_file.get('reddit', 'password'))
+
+youtubes=cfg_file.getlist("youtube", "channels")
+
+with open("lastCheck","r") as f:
+    lastUpdate=pickle.load(f)
+
+print "Checking youtube for updates since %s" % lastUpdate
+
+for youtube in youtubes:
+    print "Checking "+youtube
+    
+    
+    
+with open("lastCheck","w") as f:
+    pickle.dump(datetime.now(),f)
