19c728a2e9d455f2808328bd290df4d4b2b7cdb9
nekosune <kat.swales@nekokittygames.com>
2013-07-15 14:18:54 +0100

    started on the reddit side of the bot

---
+++ src/yrconfigparser.py
@@ -0,0 +1,9 @@
+from ConfigParser import ConfigParser
+
+class MyConfigParser(ConfigParser):
+    def getlist(self,section,option):
+        value = self.get(section,option)
+        return list(filter(None, (x.strip() for x in value.splitlines())))
+
+    def getlistint(self,section,option):
+        return [int(x) for x in self.getlist(section,option)]
